bd0010cb319daa2ac35a23e0c06848336679fd8d
Lacey <lacey.griffith@g.austincc.edu>
2021-06-27 22:18:17 -0500

    add footer, add styling

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -37,7 +37,7 @@ const Projects = () => {
             <div className='container'>
             <div className='row'>
             {projects.map(project => (
-                <div className='card-group col col-sm-auto col-md-auto col-lg-auto mt-3 mb-3 mx-auto align-self-center'>
+                <div className='card-group col col-sm-auto col-md-auto col-lg-auto mt-3 mb-3 mx-auto align-self-center' key={project.name}>
                 <div className='card results-card col-sm-4 py-2 h-200' style={{"width": "28rem", "height": "36rem"}}>
                 <h5>{project.image}</h5>
                 <div className='card-body'>
